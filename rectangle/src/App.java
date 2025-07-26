dd151b4d5052d43c595f71cb80324b8dbaccd269
Julia Pinheiro <julia.santosspinheiro@gmail.com>
2025-07-25 23:03:42 -0300

    Revert "Adicionando arquivos sobre interface e factory - Animals"
    This reverts commit 65014af69288d9cd7986d432b32ace85d88d5ed0.

--- rectangle/src/App.java
+++
@@ -1,8 +0,0 @@
-import model.entitis.Rectangle;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Rectangle rectangle1 = new Rectangle(15.00, 10.00);
-        System.out.println(rectangle1);
-    }
-}
